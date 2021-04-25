bfab20871d7ef883fa96c20eb4f7be4768726262
SaelKhlouf <sael.khlouf@gmail.com>
2021-04-26 00:08:27 +0300

    Add project files.

---
+++ FunWithAsync/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunWithAsync.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("v1/sync")]
+        public string GetSync()
+        {
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            return "hello sync";
+        }
+
+        [HttpGet("v1/async")]
+        public async Task<string> GetAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            return "hello async";
+        }
+
+
+    }
+}
